78c4bfb01b5e1cb092555b9c2e5370995d244cde
kbc-kato <kbc.kato@gmail.com>
2021-10-19 18:48:29 +0900

    20211019

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <html>
-<head><title>PHP HAISHA</title>
-<style type="text/css">
-    table{
-        border-color:skyblue;
-        border-style:solid;
-        boder-widht:1px;
-        width:1000px;
-        }
-    .hdr{background-color:gainsboro}
-</style>
-</head>
-<body>
-
-<caption>社員ログイン</caption>
-<br>
-
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scele=1">
+        <link rel="stylesheet" href="base.css">
+        <link rel="stylesheet" media= "screen and (max-width:800px)" href="base_sp.css">
+        <title>株式会社 高崎リビング</title>
+    </head>
+    <body>
+        <div id="pagebody">
+        <!-- ヘッダー -->
+            <div id = "header">
+                <h1>社員ログイン</h1>
+            </div>
+            <br><br>
 <?php
-
+    print "<div id='login'>";
     print "<form method='POST' action='login_check.php'>";
     print "社員cd<br>";
     print "<input type ='text' name = 'code'>";
-    print "<br>";
     print "<input type ='submit' value = 'ログイン'>";
     print "</form>";
-
+    print "<hr>";
+    print "</div>";
 ?>
-
-</body>
+        </div>
+    </body>
 </html>
