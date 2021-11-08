6b863fafd562f096e62c20212e4a8a707ca1df2e
kbc-kato <kbc.kato@gmail.com>
2021-11-08 18:21:35 +0900

    20211108_14

--- haisha_desp - コピー.php
+++ haisha_desp - コピー.php
@@ -171,16 +171,16 @@ EOF;
 
 
 // Using default PHP curl library
-//$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=H3QW2E59S8VDR0846YFAT5460NMJUGBIO7LC719XP12K3Z&content=http://www.wikipedia.org&margin=10&html_width=1024&title=My PDF Title');
-//
-//$response = curl_exec($ch);
-//curl_close($ch);
-//
-//var_dump($response);
+$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=H3QW2E59S8VDR0846YFAT5460NMJUGBIO7LC719XP12K3Z&content=https://tl-haisha.herokuapp.com/haisha_desp.php&margin=10&html_width=1024&title=My PDF Title');
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
 
 
  
-    $tcpdf->writeHTML($html);
+//    $tcpdf->writeHTML($html);
 //    $tcpdf->Output("haisha.pdf");
 }
 catch (exception $e)
