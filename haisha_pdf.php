406135533ae0089276ef3de4af34a3f6f82ab8d2
kbc-kato <kbc.kato@gmail.com>
2021-11-08 16:42:15 +0900

    20211108_09

---
+++ haisha_pdf.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+// Using default PHP curl library
+$ch = curl_init('https://webtopdf.expeditedaddons.com/?api_key=H3QW2E59S8VDR0846YFAT5460NMJUGBIO7LC719XP12K3Z&content=http://www.wikipedia.org&margin=10&html_width=1024&title=My PDF Title');
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
+
+
+?>
