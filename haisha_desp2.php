86f54ce2bf31cf272a5bf27c20b057961b29bafc
kbc-kato <kbc.kato@gmail.com>
2021-11-10 10:04:04 +0900

    20211110_04

--- haisha_desp2.php
+++ haisha_desp2.php
@@ -111,18 +111,18 @@ try
     <div id='hiha'>
     <table class= 'tbl'>
     <tr>
-    <th class='tbl-td'>現場名</th>
-    <th class='tbl-td'>搬入日</th>
-    <th class='tbl-td'>時間</th>
-    <th class='tbl-td'>実車会社</th>
-    <th class='tbl-td'>車輛</th>
-    <th class='tbl-td'>車番</th>
-    <th class='tbl-td'>運転者</th>
-    <th class='tbl-td'>TEL</th>
-    <th class='tbl-td'>荷受人</th>
-    <th class='tbl-td'TEL</th>
-    <th class='tbl-td'>搬入会社</th>
-    <th class='tbl-td'>人工</th>
+    <th class="tbl-td">現場名</th>
+    <th class="tbl-td">搬入日</th>
+    <th class="tbl-td">時間</th>
+    <th class="tbl-td">実車会社</th>
+    <th class="tbl-td">車輛</th>
+    <th class="tbl-td">車番</th>
+    <th class="tbl-td">運転者</th>
+    <th class="tbl-td">TEL</th>
+    <th class="tbl-td">荷受人</th>
+    <th class="tbl-td">TEL</th>
+    <th class="tbl-td">搬入会社</th>
+    <th class="tbl-td">人工</th>
     </tr>
 EOF;
 //print "debug1:";
@@ -161,18 +161,18 @@ EOF;
 
         $html .=<<< EOF
         <tr>
-        <td class='tbl-td'>$hanmry1</td>
-        <td class='tbl-td'>$hahiha</td>
-        <td class='tbl-td'>$hatmha</td>
-        <td class='tbl-td'>$hacdunjinm</td>
-        <td class='tbl-td'>$shnmsh</td>
-        <td class='tbl-td'>$hanosh</td>
-        <td class='tbl-td'>$hanmdrji</td>
-        <td class='tbl-td'>$hatldrji</td>
-        <td class='tbl-td'>$hanmuk</td>
-        <td class='tbl-td'>$hatluk</td>
-        <td class='tbl-td'>$haryha</td>
-        <td class='tbl-td'>$hehanytani</td>
+        <td class="tbl-td">$hanmry1</td>
+        <td class="tbl-td">$hahiha</td>
+        <td class="tbl-td">$hatmha</td>
+        <td class="tbl-td">$hacdunjinm</td>
+        <td class="tbl-td">$shnmsh</td>
+        <td class="tbl-td">$hanosh</td>
+        <td class="tbl-td">$hanmdrji</td>
+        <td class="tbl-td">$hatldrji</td>
+        <td class="tbl-td">$hanmuk</td>
+        <td class="tbl-td">$hatluk</td>
+        <td class="tbl-td">$haryha</td>
+        <td class="tbl-td">$hehanytani</td>
         </tr>
 EOF;
 //print "debug2:";
