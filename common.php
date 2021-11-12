2349ceb8d2dbafe567e28abd53c4dfd86ecccb39
kbc-kato <kbc.kato@gmail.com>
2021-11-12 17:29:10 +0900

    20211112_38

--- common.php
+++ common.php
@@ -2,11 +2,13 @@
 
 function sanitize($before)
     {
+print "before=".$before;
 
         foreach($before as $key => $value)
         {
             $after[$key] = htmlspecialchars($value,ENT_QUOTES,"UTF-8");
         }
+print "after=".$after;
         return $after;    
     }
     
