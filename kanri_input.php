da13c665fb828ebba02fc4892cd6fced2f241703
kbc-kato <kbc.kato@gmail.com>
2021-11-12 09:55:08 +0900

    20211112_03

--- kanri_input.php
+++ kanri_input.php
@@ -29,58 +29,58 @@ ini_set( 'error_reporting', E_ALL );
     
     print "<table class='kanri_tbl'>";
     print "<tr>";
-    print "<th>配車日 :</th>";
+    print "<th>"."配車日"."</th>";
     print "<td>;";
     print "<input type ='date' name = 'hiha' form = 'main'></label>";
     print "</td>";
     print "</tr>";
 
     print "<tr>";
-    print "<th>車番 :</th>";
+    print "<th>"."車番"."</th>";
     print "<td>;";
     print pulldown_shban();
     print "</td>";
     print "</tr>";
 
     print "<tr>";
-    print "<th>運送会社 :</th>";
+    print "<th>"."運送会社"."</th>";
     print "<td>;";
     print pulldown_unsm();
     print "</td>";
     print "</tr>";
 
     print "<tr>";
-    print "<th>運転者 :</th>";
+    print "<th>"."運転者"."</th>";
     print pulldown_drvm();
     print "</td>";
     print "</tr>";
 
     print "<tr>";
-    print "<th>行先1 :</th>";
+    print "<th>"."行先1"."</th>";
     print "<td>;";
     print "<input type ='text' name = 'nmry1' size='40' maxlength='40' form = 'main'>";
-    print " 着時間 :";
+    print " 着時間 ";
     print "<input type ='time' name = 'tmha1' form = 'main'>";
     print "</td>";
     print "</tr>";
 
     print "<tr>";
-    print "<th>備考 :</th>";
+    print "<th>"."備考"."</th>";
     print "<td>;";
     print "<textarea name = 'biko1' cols='40' rows='5' form = 'main'></textarea>";
     print "</td>";
     print "</tr>";
 
     print "<tr>";
-    print "<th>行先2 :</th>";
+    print "<th>"."行先2"."</th>";
     print "<input type ='text' name = 'nmry2' size='40' maxlength='40' form = 'main'>";
-    print " 着時間 :";
+    print " 着時間 ";
     print "<input type ='time' name = 'tmha2' form = 'main'>";
     print "</td>";
     print "</tr>";
 
     print "<tr>";
-    print "<th>備考 :</th>";
+    print "<th>"."備考"."</th>";
     print "<td>;";
     print "<textarea name = 'biko2' cols='40' rows='5' form = 'main'></textarea>";
     print "</td>";
