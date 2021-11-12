589fc03b0947b9a2981c99dcd9566a5233b82b07
kbc-kato <kbc.kato@gmail.com>
2021-11-12 11:15:55 +0900

    20211112_12

--- kanri_input.php
+++ kanri_input.php
@@ -49,7 +49,7 @@ ini_set( 'error_reporting', E_ALL );
     print "<label>"."行先1"."<br>";
     print "<input type ='text' name = 'nmry1' size='40' maxlength='40' form = 'main'>";
     print "</label>";
-    print "<label>"."着時間 "."<br>";
+    print "<label>"."着時間 ";
     print "<input type ='time' name = 'tmha1' form = 'main'>";
     print "</label>";
     print "<br>";
@@ -62,7 +62,7 @@ ini_set( 'error_reporting', E_ALL );
     print "<label>"."行先2"."<br>";
     print "<input type ='text' name = 'nmry2' size='40' maxlength='40' form = 'main'>";
     print "</label>";
-    print "<label>"."着時間 "."<br>";
+    print "<label>"."着時間 ";
     print "<input type ='time' name = 'tmha2' form = 'main'>";
     print "</label>";
     print "<br>";
@@ -81,39 +81,46 @@ ini_set( 'error_reporting', E_ALL );
 ?>
         </div>
 
-        <script type="text/javascript">
-// ▼HTMLの読み込み直後に実行：
-document.addEventListener('DOMContentLoaded', function() {
+<!-- ========================================================= -->
+<!-- ▼2段階の連動プルダウンメニューを実現するJavaScriptソース -->
+<!-- ========================================================= -->
+<script type="text/javascript">
 
-   // ▼サブBOXを全て非表示にする
-   var allSubBoxes = document.getElementsByClassName("subbox");
-   for( var i=0 ; i<allSubBoxes.length ; i++) {
-      allSubBoxes[i].style.display = 'none';
-   }
+	// HTMLの読み込み直後に実行：
+	document.addEventListener('DOMContentLoaded', function() {
 
-   // ▼全てのプルダウンボックスごとに処理
-   var mainBoxes = document.getElementsByClassName('pulldownset');
-   for( var i=0 ; i<mainBoxes.length ; i++) {
-   
-      var mainSelect = mainBoxes[i].getElementsByClassName("mainselect");   // メインのプルダウンメニュー（※後でvalue属性値を参照するので、select要素である必要があります。）
-      mainSelect[0].onchange = function () {
-         // ▼同じ親要素に含まれている全サブBOXを消す
-         var subBox = this.parentNode.getElementsByClassName("subbox");   // 同じ親要素に含まれる.subbox（※select要素に限らず、どんな要素でも構いません。）
-         for( var j=0 ; j<subBox.length ; j++) {
-            subBox[j].style.display = 'none';
-         }
-   
-         // ▼指定されたサブBOXだけを表示する
-         if( this.value ) {
-            var targetSub = document.getElementById( this.value );   // 「メインのプルダウンメニューで選択されている項目のvalue属性値」と同じ文字列をid属性値に持つ要素を得る
-            targetSub.style.display = 'inline';
-         }
-      }
-   
-   }
+    // ▼とりあえずサブBOXを全て非表示にする（CSSで書けば早いが）
+		var allSubBoxes = document.getElementsByClassName("subbox");
+		for( var i=0 ; i<allSubBoxes.length ; i++) {
+			allSubBoxes[i].style.display = 'none';
+		}
 
-});
+		// ▼全てのプルダウンボックスごとに処理
+		var mainBoxes = document.getElementsByClassName('pulldownset');
+		for( var i=0 ; i<mainBoxes.length ; i++) {
+
+			var mainSelect = mainBoxes[i].getElementsByClassName("mainselect");	// メインのプルダウンメニュー（※後でvalue属性値を参照するので、select要素である必要があります。）
+			mainSelect[0].onchange = function () {
+				// 同じ親要素に含まれている全サブBOXを消す
+				var subBox = this.parentNode.getElementsByClassName("subbox");	// 同じ親要素に含まれる.subbox（※select要素に限らず、どんな要素でも構いません。）
+				for( var j=0 ; j<subBox.length ; j++) {
+					subBox[j].style.display = 'none';
+				}
+
+				// 指定されたサブBOXを表示する
+				if( this.value ) {
+					var targetSub = document.getElementById( this.value );	// 「メインのプルダウンメニューで選択されている項目のvalue属性値」と同じ文字列をid属性値に持つ要素を得る
+					targetSub.style.display = 'inline';
+				}
+			}
+
+		}
+
+	});
 </script>
-<!--        <script src="subbox.js" charset="utf-8"></script>   -->
+<!-- ========== -->
+<!-- ▲ここまで -->
+<!-- ========== -->
+
     </body>
 </html>
