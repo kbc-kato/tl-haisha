4efb534bd08668f9c049f69cf437cf7c21ad2738
kbc-kato <kbc.kato@gmail.com>
2021-11-12 18:16:12 +0900

    20211112_39

--- kanri_input.php
+++ kanri_input.php
@@ -46,7 +46,7 @@ ini_set( 'error_reporting', E_ALL );
 
     print "<div id='kanri'>";
 
-    print "<form method='POST' name='form2' action='kanri_check.php'>";
+    print "<form method='get' name='form2' action='kanri_check.php'>";
     
     print "配車日"."<br>";
     print "<input type ='date' name = 'hiha' value = ".$hiha.">";
