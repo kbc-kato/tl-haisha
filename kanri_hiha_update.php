76205a223421729a3535bc7010e2fbe3c096f70a
kbc-kato <kbc.kato@gmail.com>
2021-11-16 10:39:58 +0900

    20211116_02

--- kanri_hiha_update.php
+++ kanri_hiha_update.php
@@ -19,9 +19,9 @@ ini_set( 'display_errors', 1 );
 ini_set( 'error_reporting', E_ALL );
 
     require_once ("common.php");
-print "GET=".$_GET['code'];
+//print "GET=".$_GET['code'];
     $seq = $_GET['code'];
-print "seq=".$seq;
+//print "seq=".$seq;
     
     include ('userfile.php');
 
@@ -38,7 +38,7 @@ print "seq=".$seq;
     
     $PDO = null;        //データベースから切断
     $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-
+var_dump($rec);
     if ($rec==false)
     {
         exit();
