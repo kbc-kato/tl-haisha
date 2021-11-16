6da59a2e0e181637c5030194ef02ab63c14e7c33
kbc-kato <kbc.kato@gmail.com>
2021-11-16 11:44:33 +0900

    20211116_02

--- kanri_hiha_branch.php
+++ kanri_hiha_branch.php
@@ -1,11 +1,16 @@
 <?php
     if(isset($_POST['edit'])==true)
     {
+        if($_POST['code']=='')
+        {
+            header('location:kanri_seq_ng.php');
+            exit();
+        }
         if(isset($_POST['code'])==false)
         {
             header('location:kanri_seq_ng.php');
             exit();
-        } 
+        }  
         $code = $_POST['code'];
         header('location:kanri_hiha_update.php?code='.$code);
         exit();
@@ -13,6 +18,11 @@
 
     if(isset($_POST['delete'])==true)
     {
+        if($_POST['code']=='')
+        {
+            header('location:kanri_seq_ng.php');
+            exit();
+        }
         if(isset($_POST['code'])==false)
         {
             header('location:kanri_seq_ng.php');
