a6c68a76b38620c437f867e75e391075124615f9
kbc-kato <kbc.kato@gmail.com>
2021-11-17 18:03:20 +0900

    20211117_04

--- kanri_shban_desp.php
+++ kanri_shban_desp.php
@@ -197,8 +197,8 @@ ini_set( 'error_reporting', E_ALL );
     print "</table>";
 
     print "<br>";
-    print "<input type ='submit' name='edit' value='修 正' class='btn'>";
-    print "<input type ='submit' name='delete' value='削 除' class='btn'>";
+    print "<input type ='submit' name='edit' value='修 正'>";
+    print "<input type ='submit' name='delete' value='削 除'>";
     print "</form>";
     print "</div>";
     print "<br>";
