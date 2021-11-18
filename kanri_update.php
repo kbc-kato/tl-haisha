34cf43e01687ffb65d5bc28134ad2af7d9bc68b6
kbc-kato <kbc.kato@gmail.com>
2021-11-18 17:00:14 +0900

    20211118_02

--- kanri_update.php
+++ kanri_update.php
@@ -171,25 +171,29 @@ var phpSession = <?php echo json_encode($_SESSION['cdun']); ?>;
 
 
 //20211118 st
-        var mainBoxes = document.getElementsByClassName('pulldownset');
-        for( var i=0 ; i<mainBoxes.length ; i++) {
+window.onload = function () {
+	document.getElementsByName('cdun')[phpSession].onchange();
+};
 
-			var mainSelect = mainBoxes[i].getElementsByClassName("mainselect");	// メインのプルダウンメニュー（※後でvalue属性値を参照するので、select要素である必要があります。）
-
-            mainSelect[0].onload = function () {
-document.write("onload");
-                // 同じ親要素に含まれている全サブBOXを消す
-				var subBox = this.parentNode.getElementsByClassName("subbox");	// 同じ親要素に含まれる.subbox（※select要素に限らず、どんな要素でも構いません。）
-				for( var j=0 ; j<subBox.length ; j++) {
-					subBox[j].style.display = 'none';
-				}
-
-				// 指定されたサブBOXを表示する
-				if( this.value ) {
-					var targetSub = document.getElementById( this.value );	// 「メインのプルダウンメニューで選択されている項目のvalue属性値」と同じ文字列をid属性値に持つ要素を得る
-					targetSub.style.display = 'inline';
-				}
-			}
+//var mainBoxes = document.getElementsByClassName('pulldownset');
+//        for( var i=0 ; i<mainBoxes.length ; i++) {
+//
+//			var mainSelect = mainBoxes[i].getElementsByClassName("mainselect");	// メインのプルダウンメニュー（※後でvalue属性値を参照するので、select要素である必要があります。）
+//
+//            mainSelect[0].onload = function () {
+//document.write("onload");
+//                // 同じ親要素に含まれている全サブBOXを消す
+//				var subBox = this.parentNode.getElementsByClassName("subbox");	// 同じ親要素に含まれる.subbox（※select要素に限らず、どんな要素でも構いません。）
+//				for( var j=0 ; j<subBox.length ; j++) {
+//					subBox[j].style.display = 'none';
+//				}
+//
+//				// 指定されたサブBOXを表示する
+//				if( this.value ) {
+//					var targetSub = document.getElementById( this.value );	// 「メインのプルダウンメニューで選択されている項目のvalue属性値」と同じ文字列をid属性値に持つ要素を得る
+//					targetSub.style.display = 'inline';
+//				}
+//			}
 //20211118 ed
 
         };
