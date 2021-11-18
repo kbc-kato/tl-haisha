31163be8adba0d42e0ef8f5392e4a2b0929d762b
kbc-kato <kbc.kato@gmail.com>
2021-11-18 14:28:15 +0900

    20211118_02

--- kanri_update.php
+++ kanri_update.php
@@ -156,9 +156,10 @@ for( var i=0 ; i<mainBoxes.length ; i++) {
 
 var mainSelect = mainBoxes[i].getElementsByClassName("mainselect");	// メインのプルダウンメニュー（※後でvalue属性値を参照するので、select要素である必要があります。）
 //        mainSelect[0].display = function () {
-document.write("display");
+//document.write("display");
                 // 同じ親要素に含まれている全サブBOXを消す
 				var subBox = this.parentNode.getElementsByClassName("subbox");	// 同じ親要素に含まれる.subbox（※select要素に限らず、どんな要素でも構いません。）
+document.write(subBox);
 				for( var j=0 ; j<subBox.length ; j++) {
 					subBox[j].style.display = 'none';
 				}
@@ -166,7 +167,8 @@ document.write("display");
 				// 指定されたサブBOXを表示する
 				if( this.value ) {
 					var targetSub = document.getElementById( this.value );	// 「メインのプルダウンメニューで選択されている項目のvalue属性値」と同じ文字列をid属性値に持つ要素を得る
-					targetSub.style.display = 'inline';
+document.write(targetSub);
+                    targetSub.style.display = 'inline';
 				}
 			}
 //20211118 ed
