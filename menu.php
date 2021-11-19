20654fe06d72c96926dcd06a5fad9d673a75e40e
kbc-kato <kbc.kato@gmail.com>
2021-11-19 16:31:45 +0900

    20211119_03

--- menu.php
+++ menu.php
@@ -17,34 +17,37 @@
 <?php
     session_start();
     session_regenerate_id(true);            //セッションIDを変える
+?>
 
-    print "<div id='menu'>";
-    print "<p>";
-    print "<input type='button' style='width:200px;height:50px' value='配車情報　検索' onclick='haisha_top.php;'>";
+    <div id='menu'>
+    <p>
+    <input type='button' style='width:200px;height:50px' value='配車情報　検索' onclick='haisha_top.php;'>
     //    print "<a href = 'haisha_top.php'>配車情報　検索</a>";
 
+<?php
     if($_SESSION["login_kbjg"]==1)
     {
-        print "<br><br>";
-        $url='keppin_desp.php';
-        print "<button type='button' style='width:200px;height:50px' onclick='location.href=".$url."'>欠品遅れ情報</button>";
+?>
+        <br><br>
+        <button type='button' style='width:200px;height:50px' onclick='location.href="keppin_desp.php"'>欠品遅れ情報</button>
 //        print "<input type='button' style='width:200px;height:50px' value='欠品遅れ情報' onclick='keppin_desp.php;'>";
 //        print "<a href = 'keppin_desp.php'>欠品遅れ情報</a>";
-        print "<br><br>";
-        print "<input type='button' style='width:200px;height:50px' value='搬入計画一覧' onclick='ke100_top.php;'>";
+        <br><br>
+        <input type='button' style='width:200px;height:50px' value='搬入計画一覧' onclick='ke100_top.php;'>
 //        print "<a href = 'ke100_top.php'>搬入計画一覧</a>";
-        print "<br><br>";
-        print "<input type='button' style='width:200px;height:50px' value='配車状況確認' onclick='ke100_top.php;'>";
+        <br><br>
+        <input type='button' style='width:200px;height:50px' value='配車状況確認' onclick='ke100_top.php;'>
 //        print "<a href = 'kanri_top.php'>配車状況確認</a>";
+<?php
     }
-
-    print "</p>";
-
-    print "<br><br>";
-    print "<input type='button' style='width:200px;height:50px' value='ログアウト' onclick='haisha_logout.php;'>";
-//    print "<a href= 'haisha_logout.php'>ログアウト</a><br>";
-    print "</div>";
 ?>
+    </p>
+
+    <br><br>
+    <input type='button' style='width:200px;height:50px' value='ログアウト' onclick='haisha_logout.php;'>
+//    print "<a href= 'haisha_logout.php'>ログアウト</a><br>";
+    </div>
+
         </div>
     </body>
 </html>
