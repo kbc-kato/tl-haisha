86fd8ae30b7c907f542637a22f01119e30cc78d6
kbc-kato <kbc.kato@gmail.com>
2021-11-19 15:51:05 +0900

    20211119_03

--- menu.php
+++ menu.php
@@ -20,26 +20,26 @@
 
     print "<div id='menu'>";
     print "<p>";
-    print "<input type='button' style='width:200px;height:50px' value='配車情報　検索' onclick='haisha_top.php'>";
-//    print "<a href = 'haisha_top.php'>配車情報　検索</a>";
+    print "<input type='button' style='width:200px;height:50px' value='配車情報　検索' onclick='haisha_top.php;'>";
+    //    print "<a href = 'haisha_top.php'>配車情報　検索</a>";
 
     if($_SESSION["login_kbjg"]==1)
     {
         print "<br><br>";
-        print "<input type='button' style='width:200px;height:50px' value='欠品遅れ情報' onclick='keppin_desp.php'>";
+        print "<input type='button' style='width:200px;height:50px' value='欠品遅れ情報' onclick='keppin_desp.php;'>";
 //        print "<a href = 'keppin_desp.php'>欠品遅れ情報</a>";
         print "<br><br>";
-        print "<input type='button' style='width:200px;height:50px' value='搬入計画一覧' onclick='ke100_top.php'>";
+        print "<input type='button' style='width:200px;height:50px' value='搬入計画一覧' onclick='ke100_top.php;'>";
 //        print "<a href = 'ke100_top.php'>搬入計画一覧</a>";
         print "<br><br>";
-        print "<input type='button' style='width:200px;height:50px' value='配車状況確認' onclick='ke100_top.php'>";
+        print "<input type='button' style='width:200px;height:50px' value='配車状況確認' onclick='ke100_top.php;'>";
 //        print "<a href = 'kanri_top.php'>配車状況確認</a>";
     }
 
     print "</p>";
 
     print "<br><br>";
-    print "<input type='button' style='width:200px;height:50px' value='ログアウト' onclick='haisha_logout.php'>";
+    print "<input type='button' style='width:200px;height:50px' value='ログアウト' onclick='haisha_logout.php;'>";
 //    print "<a href= 'haisha_logout.php'>ログアウト</a><br>";
     print "</div>";
 ?>
